a7321535bfd6bf0d2b13e012a3101257fce0b446
fairlite2u <pyl95001@byui.edu>
2022-02-03 09:41:09 -0700

    team activity 05

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 const mongoose = require('mongoose');
 const cors = require('cors')
+const session = require('express-session');
 
 const app = express();
 
@@ -25,6 +26,7 @@ const ta01Routes = require('./routes/ta01');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
+const ta05Routes = require('./routes/ta05');
 
 app
   .use(express.static(path.join(__dirname, 'public')))
@@ -35,11 +37,19 @@ app
   // For view engine as hbs (Handlebars)
   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
   //.set('view engine', 'hbs')
+  app.use(
+    session({ 
+      secret: 'my secret', 
+      resave: false, 
+      saveUninitialized: false, 
+    })
+  )
   .use(bodyParser.urlencoded({ extended: false })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
+  .use('/ta05', ta05Routes)
   .get('/', (req, res, next) => {
     // This is the primary index, always handled last.
     res.render('pages/index', {
